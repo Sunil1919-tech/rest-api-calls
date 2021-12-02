4801638a7bfb7f3c678ee5741319b3c5c85ff632
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-02 07:12:08 +0530

    Added: Displaying hello message by passing the name as pathVariable

--- src/main/java/com/bridgelabz/restapispringapp/controller/HelloRestApiController.java
+++ src/main/java/com/bridgelabz/restapispringapp/controller/HelloRestApiController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapispringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class HelloRestApiController {
         return "Hello" + Mark + " " + "from bridgelabz";
     }
 
-
+    @GetMapping(value = "/path{name}")
+    public String sayHello(
+            @PathVariable String name
+    ) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
