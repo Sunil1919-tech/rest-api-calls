c6e863e67c7d27f5bcf5bf6b0bd29e3a8aaea21c
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-02 07:07:10 +0530

    Added: using getMethod and passing name as QueryParam

--- src/main/java/com/bridgelabz/restapispringapp/controller/HelloRestApiController.java
+++ src/main/java/com/bridgelabz/restapispringapp/controller/HelloRestApiController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapispringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,13 @@ public class HelloRestApiController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+
+    @GetMapping(value = "/query")
+    public String sayHelloMark(
+            @RequestParam(value = "name") String Mark
+    ) {
+        return "Hello" + Mark + " " + "from bridgelabz";
+    }
+
+
 }
